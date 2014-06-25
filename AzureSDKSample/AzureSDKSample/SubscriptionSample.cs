d6c0e9f3b822be453ac86312ba2e242b345b85c8
Rajdeep Dua <dua_rajdeep@yahoo.com>
2014-06-25 12:26:48 +0530

    Converted single file into multiple files

---
+++ AzureSDKSample/AzureSDKSample/SubscriptionSample.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Management.Compute;
+using Microsoft.WindowsAzure.Management;
+using Microsoft.WindowsAzure.Subscriptions;
+using Microsoft.WindowsAzure.Management.Models;
+
+namespace AzureSDKSample
+{
+    class SubscriptionSample
+    {
+        public static void ListOperations()
+        {
+            ManagementClient client = Util.getManagementClient();
+
+            var utcStart = new DateTime(2014, 04, 01, 0, 0, 0).ToUniversalTime();
+            var utcEnd = new DateTime(2014, 06, 01, 0, 0, 0).ToUniversalTime();
+
+            var operations = client.Subscriptions.ListOperations(
+                new SubscriptionListOperationsParameters()
+                {
+                    StartTime = utcStart,
+                    EndTime = utcEnd
+                });
+
+            foreach (var op in operations.SubscriptionOperations)
+            {
+                Console.WriteLine(string.Format("Operation name:{0}, Operation id:{1}",
+                      op.OperationName, op.OperationId));
+            }
+            Console.WriteLine("---------------------------------------");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AzureSDKSample/AzureSDKSample/Util.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Management.Compute;
+using Microsoft.WindowsAzure.Management;
+using Microsoft.WindowsAzure;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AzureSDKSample
+{
+    class Util
+    {
+        public static ManagementClient getManagementClient()
+        {
+            var creds = GetCredentials();
+            ManagementClient client = CloudContext.Clients.CreateManagementClient(creds);
+            return client;
+        }
+
+        public static ComputeManagementClient getComputeManagementClient()
+        {
+            var creds = GetCredentials();
+            ComputeManagementClient client = CloudContext.Clients.CreateComputeManagementClient(creds);
+            return client;
+        }
+
+
+        public static CertificateCloudCredentials GetCredentials()
+        {
+            //Create a new instance of the X509 certificate
+            X509Certificate2 cert = new X509Certificate2(
+              Program.CERT_PATH);
+
+            //Pass your subscription id and the certificate to the CertificateCloudCredentials 
+            //class constructor
+            CertificateCloudCredentials creds = new CertificateCloudCredentials(
+              Program.SUBS_ID, cert);
+
+            return creds;
+        }
+    }
+}
